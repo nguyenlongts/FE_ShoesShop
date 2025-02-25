0902ea2f0835b1abf3c8dc7e1639a33b09ca73cc
Long <Long@ADMIN-PC>
2025-02-25 20:44:37 +0700

    fix after admin logout can back to admin panel

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,40 @@
-import { Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import HomePage from './pages/HomePage';
-import ProductDetail from './pages/ProductDetail';
-import CartPage from './pages/CartPage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import AdminLayout from './components/layouts/AdminLayout';
-import AdminUsersPage from './pages/admin/AdminUsersPage';
-import AdminOrderPage from './pages/admin/AdminOrderPage';
-import AdminProductPage from './pages/admin/AdminProductPage';
-import AdminCategoryPage from './pages/admin/AdminCategoryPage';
-import AdminBrandPage from './pages/admin/AdminBrandPage';
-import AdminReportPage from './pages/admin/AdminReportPage';
-import AdminOrderDetailPage from './pages/admin/AdminOrderDetailPage';
-import AdminProductDetailPage from './pages/admin/AdminProductDetailPage';
-import VoucherPage from './pages/VoucherPage';
-import OrderHistory from './pages/OrderHistory';
-import CheckoutPage from './pages/CheckoutPage';
-import OrderSuccessPage from './pages/OrderSuccessPage';
-import { Toaster } from 'react-hot-toast';
-import AdminSizePage from './pages/admin/AdminSizePage';
-import AdminColorPage from './pages/admin/AdminColorPage';
-import AdminCouponPage from './pages/admin/AdminCouponPage';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import ProductDetail from "./pages/ProductDetail";
+import CartPage from "./pages/CartPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import AdminLayout from "./components/layouts/AdminLayout";
+import AdminUsersPage from "./pages/admin/AdminUsersPage";
+import AdminOrderPage from "./pages/admin/AdminOrderPage";
+import AdminProductPage from "./pages/admin/AdminProductPage";
+import AdminCategoryPage from "./pages/admin/AdminCategoryPage";
+import AdminBrandPage from "./pages/admin/AdminBrandPage";
+import AdminReportPage from "./pages/admin/AdminReportPage";
+import AdminOrderDetailPage from "./pages/admin/AdminOrderDetailPage";
+import AdminProductDetailPage from "./pages/admin/AdminProductDetailPage";
+import VoucherPage from "./pages/VoucherPage";
+import OrderHistory from "./pages/OrderHistory";
+import CheckoutPage from "./pages/CheckoutPage";
+import OrderSuccessPage from "./pages/OrderSuccessPage";
+import { Toaster } from "react-hot-toast";
+import AdminSizePage from "./pages/admin/AdminSizePage";
+import AdminColorPage from "./pages/admin/AdminColorPage";
+import AdminCouponPage from "./pages/admin/AdminCouponPage";
+const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem("token");
+  const userStr = localStorage.getItem("user");
 
+  if (!token || !userStr) {
+    // Chặn và chuyển hướng tới trang đăng nhập
+    return <Navigate to="/signin" replace />;
+  }
+
+  // Nếu có token, render children
+  return children;
+};
 function App() {
   return (
     <>
@@ -34,23 +45,29 @@ function App() {
         <Route path="/register" element={<RegisterPage />} />
 
         {/* Client Routes - With Header/Footer */}
-        <Route path="/*" element={
-          <div className="font-nike min-h-screen flex flex-col">
-            <Header />
-            <main className="flex-grow">
-              <Routes>
-                <Route index element={<HomePage />} />
-                <Route path="product/:id" element={<ProductDetail />} />
-                <Route path="cart" element={<CartPage />} />
-                <Route path="myvoucher" element={<VoucherPage />} />
-                <Route path="history" element={<OrderHistory />} />
-                <Route path="checkout" element={<CheckoutPage />} />
-                <Route path="order-success/:orderId" element={<OrderSuccessPage />} />
-              </Routes>
-            </main>
-            <Footer />
-          </div>
-        } />
+        <Route
+          path="/*"
+          element={
+            <div className="font-nike min-h-screen flex flex-col">
+              <Header />
+              <main className="flex-grow">
+                <Routes>
+                  <Route index element={<HomePage />} />
+                  <Route path="product/:id" element={<ProductDetail />} />
+                  <Route path="cart" element={<CartPage />} />
+                  <Route path="myvoucher" element={<VoucherPage />} />
+                  <Route path="history" element={<OrderHistory />} />
+                  <Route path="checkout" element={<CheckoutPage />} />
+                  <Route
+                    path="order-success/:orderId"
+                    element={<OrderSuccessPage />}
+                  />
+                </Routes>
+              </main>
+              <Footer />
+            </div>
+          }
+        />
 
         {/* Admin Routes */}
         <Route path="/admin/*" element={<AdminLayout />}>
