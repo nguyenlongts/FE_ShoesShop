783b04856c928c84b48fc7c82d7d2bb63fd71083
Long <Long@ADMIN-PC>
2025-03-25 21:43:24 +0700

    update

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { toast } from "react-hot-toast";
 import { jwtDecode } from "jwt-decode";
-
+const API_URL = process.env.REACT_APP_API_URL;
 const LoginPage = () => {
   const navigate = useNavigate();
   const [credentials, setCredentials] = useState({
@@ -22,7 +22,7 @@ const LoginPage = () => {
 
     try {
       const { data } = await axios.post(
-        "http://localhost:5258/api/Auth/login",
+        `${API_URL}/api/Auth/login`,
         {
           Username: credentials.username.trim(),
           Password: credentials.password,
