23d3153f66185d1920c3d471d853bb115d9089f1
Long <Long@ADMIN-PC>
2025-03-25 21:49:46 +0700

    update

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,7 +3,8 @@ import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { toast } from "react-hot-toast";
 import { jwtDecode } from "jwt-decode";
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
+console.log(API_URL);
 const LoginPage = () => {
   const navigate = useNavigate();
   const [credentials, setCredentials] = useState({
